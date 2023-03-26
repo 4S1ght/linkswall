8d0d570af03704c30ff34f8fa14305257ae9504b
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-03-26 20:45:14 +0200

    Basic page layout

---
+++ src/components/Feed.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+</script>
+
+<section class="feed-wrapper">
+    <div class="feed">
+        <slot/>
+    </div>
+</section>
+
+<style lang="scss">
+
+    section.feed-wrapper {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin-top: 90px;
+
+        .feed {
+            max-width: 1400px;
+            width: calc(100% - 40px);
+            border-radius: 10px;
+            border: solid 1px #0000001c;
+            margin-bottom: 100px;
+            padding: 20px;
+        }
+        
+    }
+
+</style>
