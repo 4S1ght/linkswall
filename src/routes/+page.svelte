2ecfc8ebd1bac1dfeb808227115661f0d256d090
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-03-27 12:56:46 +0200

    Added GET /api/links for landing page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,23 @@
     import Feed from '../components/Feed.svelte'
     import Post from '../components/Post.svelte'
 
+    import type { Link } from '../links';
+
+    async function getLinks() {
+        const req = await fetch('/api/links')
+        return await req.json() as Link[]
+    }
+
 </script>
 
 <Feed>
-    {#each new Array(10) as item, i}
-        <Post link="https://google.com" title="(x{i}) Google market share down " description="Once upon a time at google..." votes={1337}/>
-    {/each}
+    {#await getLinks()}
+        <p>Loading...</p>
+    {:then links}
+        {#each links as link, i}
+            <Post {...link}/>
+        {/each}
+    {:catch error}
+        <p>Something went wrong: {error.message}</p>
+    {/await}
 </Feed>
