376bd906bc1b968b3f57170b1ab59ab940b8e5e9
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-03-28 21:47:25 +0200

    Moved link upvote tresholds into a separate config

--- src/routes/api/links/+server.ts
+++ src/routes/api/links/+server.ts
@@ -1,10 +1,11 @@
 
 import { error } from '@sveltejs/kit';
 import Links from '../../../links';
+import config from '../../../config.json';
 
 export async function GET() {
 
-    const links = (await Links.list()).filter(x => x.votes >= 5)
+    const links = (await Links.list()).filter(x => x.votes >= config.link_upvote_threshold)
 
 	return new Response(
         JSON.stringify(links),
--- src/routes/api/trendinglinks/+server.ts
+++ src/routes/api/trendinglinks/+server.ts
@@ -1,10 +1,13 @@
 
 import { error } from '@sveltejs/kit';
 import Links from '../../../links';
+import config from '../../../config.json';
 
 export async function GET() {
 
-    const links = (await Links.list()).filter(x => x.votes >= 50).sort((a, b) => a.votes - b.votes)
+    const links = (await Links.list())
+        .filter(x => x.votes >= config.trending_link_vote_threshold)
+        .sort((a, b) => a.votes - b.votes)
 
 	return new Response(
         JSON.stringify(links),
