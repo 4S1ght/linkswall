1fbcc7813c36aa9cd7729733b6d47d33b3f543cc
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-03-28 22:13:10 +0200

    /trending links are now sorted by the upvote intensity

--- src/routes/api/trendinglinks/+server.ts
+++ src/routes/api/trendinglinks/+server.ts
@@ -1,13 +1,22 @@
 
-import { error } from '@sveltejs/kit';
 import Links from '../../../links';
 import config from '../../../config.json';
 
+import type { Link } from '../../../links';
+
+const day = 24 * 60 * 60 * 1000
+
 export async function GET() {
 
-    const links = (await Links.list())
+    const now = Date.now()
+
+    const links = ((await Links.list()) as Array<Link & { vote_intensity: number }>)
         .filter(x => x.votes >= config.trending_link_vote_threshold)
-        .sort((a, b) => a.votes - b.votes)
+        .map(x => {
+            const age = Math.max(1, Math.round((now - x.timestamp) / day))
+            return { ...x, vote_intensity: x.votes / age }
+        })
+        .sort((a, b) => b.vote_intensity - a.vote_intensity)
 
 	return new Response(
         JSON.stringify(links),
