4ef4785d9ba4308a7d697414b67830faba5b674b
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-03-31 13:34:58 +0200

    Added a static adapter and turned off SSR

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,15 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+
+	adapter: adapter({
+		pages: 'build',
+		assets: 'build',
+		fallback: null,
+		precompress: false,
+		strict: true
+	})
 };
 
 export default config;
